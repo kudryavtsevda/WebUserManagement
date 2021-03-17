455793abd9ac1d922c027edd28bc6f42d52ec496
Dmitrii Kudriavtsev <diak3@yandex.ru>
2021-03-17 14:54:31 +0400

    removed unused usings

--- WebUserManagement.Api/DTO/UserResponse.cs
+++ WebUserManagement.Api/DTO/UserResponse.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace WebUserManagement.Api.DTO
+﻿namespace WebUserManagement.Api.DTO
 {
     public class UserResponse
     {
