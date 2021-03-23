26ed2aa965f831565e91f42dec1a0eb6635494a3
Dmitrii Kudriavtsev <diak3@yandex.ru>
2021-03-24 01:30:31 +0400

    move logic from index.js to app.jsx

---
+++ WebUserManagement.UI.ReactJS/Scripts/Home/react/App.jsx
@@ -0,0 +1,49 @@
+﻿import React from 'react';
+import 'materialize-css';
+import { Button, Icon } from 'react-materialize';
+import { MainScreen } from './MainScreen.jsx';
+import { EditUser } from './EditUser.jsx';
+import { AddUser } from './AddUser.jsx';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+
+export function App(props) {
+    return (
+        <Router>
+            <div>
+                <ul>
+                    <li>
+                        <Link to="/">
+                            <Button icon={<Icon right>list</Icon>} flat node="button" waves="light">
+                                Get all users
+                                 </Button>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/createuser">
+                            <Button icon={<Icon right>person_add</Icon>} flat node="button" waves="light">
+                                Add user
+                                 </Button>
+                        </Link>
+                    </li>
+                </ul>
+                <hr />
+                <Switch>
+                    <Route exact path="/">
+                        <MainScreen />
+                    </Route>
+                    <Route path="/createuser">
+                        <AddUser />
+                    </Route>
+                    <Route path="/edituser/:id">
+                        <EditUser />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    )
+}
