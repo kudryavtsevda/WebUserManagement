438681e3de4ece7234bf1c887b7ba2e9a03e18af
Dmitrii Kudriavtsev <diak3@yandex.ru>
2021-03-24 01:07:10 +0400

    remove unncesssary projects

--- WebApplication2/Scripts/Home/react/http-common.js
+++
@@ -1,8 +0,0 @@
-﻿import axios from "axios";
-
-export default axios.create({
-    baseURL: "https://localhost:44397/api/users/",
-    headers: {
-        "Content-type": "application/json"
-    }
-});
