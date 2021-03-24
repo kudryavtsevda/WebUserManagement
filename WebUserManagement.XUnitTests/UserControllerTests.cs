65f8a3fc545b378658b0f4a36c86534c6dd5bc18
Dmitrii Kudriavtsev <diak3@yandex.ru>
2021-03-24 16:54:52 +0400

    small refactoring

--- WebUserManagement.XUnitTests/UserControllerTests.cs
+++ WebUserManagement.XUnitTests/UserControllerTests.cs
@@ -1,9 +1,7 @@
 ﻿using FakeItEasy;
 using FluentAssertions;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http.Results;
 using WebUserManagement.Api.Controllers;
